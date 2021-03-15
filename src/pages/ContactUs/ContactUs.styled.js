0665c49dfb6f177bdb4bdaed16989ac53d579228
Talmacel Marian-Silviu <magicace1988@gmail.com>
2021-03-15 22:32:33 +0000

    removed some code

--- src/pages/ContactUs/ContactUs.styled.js
+++ src/pages/ContactUs/ContactUs.styled.js
@@ -1,9 +1,1 @@
 import styled from 'styled-components';
-
-const colors = (props) => props.theme.colors.contactUs;
-
-export const Container = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-`;
