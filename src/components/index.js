eb317a385804599e9dbb7e31f1e68f79db7cae30
Talmacel Marian-Silviu <magicace1988@gmail.com>
2021-03-17 22:06:53 +0000

    finished wave animation

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import AboutSection from './AboutSection';
 import FaqSection from './FaqSection';
 import ServicesSection from './ServicesSection';
 import Nav from './Nav';
+import Wave from './Wave';
 
-export { AboutSection, FaqSection, ServicesSection, Nav };
+export { AboutSection, FaqSection, ServicesSection, Nav, Wave };
