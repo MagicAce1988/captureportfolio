20fef3614ea5a9dc3eff7beef48b20ce20bd642a
Talmacel Marian-Silviu <magicace1988@gmail.com>
2021-03-22 23:06:06 +0000

    finished our work animations and contact us

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,16 +1,48 @@
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../../animation';
+import { pageAnimation, titleAnimation } from '../../animation';
+import {
+  Circle,
+  ContactContainer,
+  Hide,
+  Social,
+  Title,
+} from './ContactUs.styled';
 
 const ContactUs = ({ ...props }) => {
   return (
-    <motion.div
+    <ContactContainer
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
+      style={{ background: '#fff' }}
     >
-      Contact Us
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnimation}>Get in touch</motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle />
+            <h2>Send us a message</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle />
+            <h2>Send an email</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnimation}>
+            <Circle />
+            <h2>Social Media</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactContainer>
   );
 };
 
